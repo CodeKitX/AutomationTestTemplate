c555f3c05adc446cb7242aaca84e717a262290e2
Łukasz Gościniak <codekitx@gmail.com>
2025-12-11 10:32:30 +0100

    simple ApiClient

--- src/main/java/interview/api/ApiClient.java
+++ src/main/java/interview/api/ApiClient.java
@@ -1,10 +1,21 @@
 package interview.api;
 
+import static io.restassured.RestAssured.given;
+import io.restassured.response.Response;
+
+
 public class ApiClient {
     private final String baseUrl;
 
     public ApiClient(String baseUrl) {
         this.baseUrl = baseUrl;
+    }
+
+    public Response get(String path) {
+        return given()
+                .baseUri(baseUrl)
+                .when()
+                .get(path);
     }
 
     public String getBaseUrl() {
